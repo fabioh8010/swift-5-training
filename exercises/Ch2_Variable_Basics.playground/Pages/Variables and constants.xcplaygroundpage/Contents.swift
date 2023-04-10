f77bb7ad3d3307bf299077cab72d3973555f1201
Fábio Henriques <contact@fabiohenriques.dev>
2023-04-10 18:46:28 +0100

    feat: variables exercise

--- exercises/Ch2_Variable_Basics.playground/Pages/Variables and constants.xcplaygroundpage/Contents.swift
+++ exercises/Ch2_Variable_Basics.playground/Pages/Variables and constants.xcplaygroundpage/Contents.swift
@@ -15,4 +15,8 @@ Declaring variables in Swift depends on how you plan to use their values in your
  [Next Topic](@next)
  
 */
+var currentHealth = 90
+let maxHealth = 100
 
+currentHealth = 13
+// maxHealth = 110
